c33d8fc4becd5b56c8fa2d3d01e623be8a82a745
rcccantos <riccocantos@gmail.com>
2025-11-05 16:53:58 +0800

    first js app yay

--- Passenger_Counter/index.js
+++ Passenger_Counter/index.js
@@ -1,20 +1,21 @@
-let count = 0
-let countEl = document.getElementById("count-el")
+let count = 0 // initialize
+let countEl = document.getElementById("count-el") // counter
+let saveEl = document.getElementById("save-el") // for printing
 // the DOM (Document Object Model) or how u use js to modify a website
+
 function increment() {
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
 function decrement() {
     count -= 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
-let saveEl = document.getElementById("save-el")
-
 function save() {
-    let countStr = " " + count + " - "
-    saveEl.innerText += countStr
-    console.log(count)
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
 }
