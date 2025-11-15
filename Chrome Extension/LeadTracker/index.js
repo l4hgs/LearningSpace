c6fd5a31aec18cd0f20f7b2554083f5021a3d607
rcccantos <riccocantos@gmail.com>
2025-11-16 07:51:46 +0800

    const + innerHTML

--- Chrome Extension/LeadTracker/index.js
+++ Chrome Extension/LeadTracker/index.js
@@ -1,5 +1,16 @@
-let inputEl = document.getElementById("input-btn")
-inputEl.addEventListener("click", function() {
-    inputEl.textContent = "Saved"
+let myLeads = ["ffja.com", "fadsfjakl.com", "kjdklfa.com"]
+// const = can't be reassigned
+const inputEl = document.getElementById("input-el") 
+const inputValue = inputEl.value
+
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputValue)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+    // innerHTML to transform string to html tags
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
 
--- prac.js
+++ prac.js
@@ -8,8 +8,20 @@
 //     console.log("- " + countries[i])
 // }
 
-let randomIndex = Math.floor(Math.random() * 3)
+/* let randomIndex = Math.floor(Math.random() * 3)
 
 let hands = ["rock", "paper", "scissors"]
 
-console.log(hands[randomIndex])
+console.log(hands[randomIndex]) */
+
+const basePrice = 520
+const discount = 120
+let shippingCost = 12
+let shippingTime = "5-12 days"
+
+shippingCost = 15
+shippingTime = "7-14 days"
+
+const fullPrice = basePrice - discount + shippingCost
+
+console.log("Total cost: " + fullPrice + ". It will arrive in " + shippingTime)
