0ce1e5ea3862277da9f0727e27f084631d44d3fe
rcccantos <riccocantos@gmail.com>
2025-11-18 13:44:57 +0800

    aw

--- prac.js
+++ prac.js
@@ -1,19 +1,3 @@
-// let countries = ["Tuvalu", "India", "USA", "Indonesia", "Monaco"]
-
-// countries.shift(); //removes first element
-// countries.unshift("Pakistan") // adds at first element
-// countries.unshift("China")
-// countries.pop()
-// for (let i = 0; i < countries.length; i++) {
-//     console.log("- " + countries[i])
-// }
-
-/* let randomIndex = Math.floor(Math.random() * 3)
-
-let hands = ["rock", "paper", "scissors"]
-
-console.log(hands[randomIndex]) */
-
 const basePrice = 520
 const discount = 120
 let shippingCost = 12
